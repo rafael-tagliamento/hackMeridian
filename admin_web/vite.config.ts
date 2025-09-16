1748e278e848d50ca74423718922cd15bc7a3a5b
Rafael Tagliamento Santana <taglirafael@gmail.com>
2025-09-16 10:05:21 -0300

    Change base path in Vite config

--- admin_web/vite.config.ts
+++ admin_web/vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: "/hackMeridian/admin",
+    base: "/hackMeridian",
     plugins: [react()],
     define: {
       global: 'globalThis',
